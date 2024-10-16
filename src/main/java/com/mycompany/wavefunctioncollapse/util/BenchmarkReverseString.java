0701ee05992aad556b7908b725e732b602680263
NhatTranMinh15 <Nhat.TranMinh@nashtechglobal.com>
2024-10-16 09:16:48 +0700

    Misc

---
+++ src/main/java/com/mycompany/wavefunctioncollapse/util/BenchmarkReverseString.java
@@ -0,0 +1,158 @@
+package com.mycompany.wavefunctioncollapse.util;
+
+import java.util.Random;
+
+public class BenchmarkReverseString {
+
+    static boolean reverseUsingStringAndBytes(String a, String b) {
+        byte[] bytes = b.getBytes();
+        int length = bytes.length;
+        int half = length / 2;
+        for (int i = 0; i < half; i++) {
+            byte t = bytes[i];
+            bytes[i] = bytes[length - 1 - i];
+            bytes[length - 1 - i] = t;
+        }
+        return a.equals(new String(bytes));
+    }
+
+    static boolean reverseUsingBytesAndBytes(String a, String b) {
+        byte[] bytesa = b.getBytes();
+        byte[] bytesb = a.getBytes();
+        int length = bytesa.length;
+        for (int i = 0; i < length; i++) {
+            if (bytesa[i] != bytesb[length - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean reverseUsingGetBytesAndGetBytes(String a, String b) {
+        int length = a.length();
+        for (int i = 0; i < length; i++) {
+            if (a.charAt(i) != b.charAt(length - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean reverseUsingStringBuilder(String a, String b) {
+        return new StringBuilder(b).reverse().toString().equals(a);
+    }
+
+    static String reverse(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+
+    static String alphabet = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+
+    static Random r = new Random();
+
+    static String makeString(int length) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append(alphabet.charAt(r.nextInt(alphabet.length())));
+        }
+        return sb.toString();
+    }
+
+    static void checkEquality(String[][] strings) {
+        System.out.println("Checking functions output:");
+        String t1, t2;
+        for (String[] string : strings) {
+            t1 = new String(string[0].getBytes());
+            t2 = new String(string[0].getBytes());
+            boolean a = reverseUsingStringAndBytes(t1, t2);
+            t1 = new String(string[0].getBytes());
+            t2 = new String(string[0].getBytes());
+            boolean b = reverseUsingBytesAndBytes(t1, t2);
+            t1 = new String(string[0].getBytes());
+            t2 = new String(string[0].getBytes());
+            boolean c = reverseUsingStringBuilder(t1, t2);
+            t1 = new String(string[0].getBytes());
+            t2 = new String(string[0].getBytes());
+            boolean d = reverseUsingGetBytesAndGetBytes(t1, t2);
+            if (a && b && c && d) {
+                continue;
+            }
+            if (!a && !b && !c && !d) {
+                continue;
+            }
+            System.out.println("Fail: " + a + " " + b + " " + c + " " + d + " " + string[0] + " " + string[1]);
+        }
+        System.out.println("Done");
+    }
+
+    static long doReverseUsingStringAndBytes(String[][] strings) {
+        long start, end;
+        start = System.nanoTime();
+        for (String[] string : strings) {
+            reverseUsingStringAndBytes(string[0], string[1]);
+        }
+        end = System.nanoTime();
+        return end - start;
+    }
+
+    static long doReverseUsingBytesAndBytes(String[][] strings) {
+        long start, end;
+        start = System.nanoTime();
+        for (String[] string : strings) {
+            reverseUsingBytesAndBytes(string[0], string[1]);
+        }
+        end = System.nanoTime();
+        return end - start;
+    }
+
+    static long doReverseUsingGetBytesAndGetBytes(String[][] strings) {
+        long start, end;
+        start = System.nanoTime();
+        for (String[] string : strings) {
+            reverseUsingGetBytesAndGetBytes(string[0], string[1]);
+        }
+        end = System.nanoTime();
+        return end - start;
+    }
+
+    static long doReverseUsingStringBuilder(String[][] strings) {
+        long start, end;
+        start = System.nanoTime();
+        for (String[] string : strings) {
+            reverseUsingStringBuilder(string[0], string[1]);
+        }
+        end = System.nanoTime();
+        return end - start;
+    }
+
+    public static void main(String[] args) {
+        int stringArrayLength = 10;
+        int stringLength = 10000;
+        double similarPosibility = 0.5;
+        int loopLength = 10;
+        String[][] strings = new String[stringArrayLength][2];
+        for (String[] string : strings) {
+            int length = r.nextInt(stringLength) + 1;
+            string[0] = makeString(length);
+            if (r.nextFloat() < similarPosibility) {
+                string[1] = reverse(string[0]);
+            } else {
+                string[1] = makeString(length);
+            }
+        }
+        checkEquality(strings);
+        long ta = 0, tb = 0, tc = 0, td = 0;
+        for (int i = 0; i < loopLength; i++) {
+            System.out.println(i);
+            // checkEquality(strings);
+            ta += doReverseUsingBytesAndBytes(strings);
+            tb += doReverseUsingStringAndBytes(strings);
+            tc += doReverseUsingStringBuilder(strings);
+            td += doReverseUsingGetBytesAndGetBytes(strings);
+        }
+        System.out.println("doReverseUsingBytesAndBytes: " + ta * 1.0 / 1000000000);
+        System.out.println("doReverseUsingStringAndBytes: " + tb * 1.0 / 1000000000);
+        System.out.println("doReverseUsingStringBuilder: " + tc * 1.0 / 1000000000);
+        System.out.println("doReverseUsingGetBytesAndGetBytes: " + td * 1.0 / 1000000000);
+    }
+}
---
+++ src/main/java/com/mycompany/wavefunctioncollapse/util/Test.java
@@ -0,0 +1,100 @@
+package com.mycompany.wavefunctioncollapse.util;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.imageio.ImageIO;
+
+public class Test {
+
+    public static void genImage() throws IOException {
+        for (int i = 0; i < 512; i++) {
+            File file = new File("src/main/resources/test/small/" + i + ".png");
+            BufferedImage image = new BufferedImage(3, 3, BufferedImage.TYPE_INT_RGB);
+            for (int j = 0; j < 9; j++) {
+                int c = (i >> j) & 1;
+                int color;
+                if (c == 1) {
+                    color = Color.GREEN.getRGB();
+                } else {
+                    color = Color.BLACK.getRGB();
+                }
+                int x = j / 3;
+                int y = j % 3;
+                image.setRGB(x, y, color);
+            }
+            ImageIO.write(image, "png", file);
+        }
+    }
+
+    public static void scaleImage() throws IOException {
+        for (int i = 0; i < 512; i++) {
+            File file = new File("src/main/resources/test/small/" + i + ".png");
+            BufferedImage image = ImageIO.read(file);
+            BufferedImage newImage = new BufferedImage(30, 30, image.getType());
+            Graphics2D g = newImage.createGraphics();
+            g.scale(10, 10);
+            g.drawImage(image, null, 0, 0);
+            File out = new File("src/main/resources/test/big/" + i + ".png");
+            ImageIO.write(newImage, "png", out);
+        }
+    }
+
+    public static void scaleDownImage() throws IOException {
+        File folder = new File("src/main/resources/test/big");
+        for (File file : folder.listFiles()) {
+            BufferedImage image = ImageIO.read(file);
+            BufferedImage newImage = new BufferedImage(3, 3, image.getType());
+            Graphics2D g = newImage.createGraphics();
+            g.scale(0.1, 0.1);
+            g.drawImage(image, null, 0, 0);
+            File out = new File("src/main/resources/test/small/" + file.getName());
+            ImageIO.write(newImage, "png", out);
+        }
+    }
+
+    public static void markImage() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("src/main/resources/test/small/\n");
+        sb.append("16\n");
+        File folder = new File("src/main/resources/test/small");
+        String up = "", right = "", down = "", left = "";
+        Map<Integer, Integer> map = Map.of(-16777216, 0, -16711936, 1);
+        int n = 0;
+        for (File file : folder.listFiles()) {
+            if (!file.getName().split("\\.")[1].equals("png")) {
+                continue;
+            }
+            BufferedImage image = ImageIO.read(file);
+            int top_left = image.getRGB(0, 0);
+            int top_middle = image.getRGB(0, 1);
+            int top_right = image.getRGB(0, 2);
+            int middle_left = image.getRGB(1, 0);
+            int middle_right = image.getRGB(1, 2);
+            int bottom_left = image.getRGB(2, 0);
+            int bottom_middle = image.getRGB(2, 1);
+            int bottom_right = image.getRGB(2, 2);
+            sb.append(file.getName()).append("\n");
+            sb.append(map.get(top_left)).append(map.get(top_middle)).append(map.get(top_right)).append(",");
+            sb.append(map.get(top_right)).append(map.get(middle_right)).append(map.get(bottom_right)).append(",");
+            sb.append(map.get(bottom_right)).append(map.get(bottom_middle)).append(map.get(bottom_left)).append(",");
+            sb.append(map.get(bottom_left)).append(map.get(middle_left)).append(map.get(top_left));
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+        File out = new File("src/main/resources/test/small/data.txt");
+        FileWriter fw = new FileWriter(out);
+        fw.write(sb.toString());
+        fw.flush();
+        fw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        markImage();
+    }
+}
